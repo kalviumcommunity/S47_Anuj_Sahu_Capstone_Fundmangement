6d0bdc849bb12ef72283e8f2bc7592f9dcbb033c
004Anuj <anuj.sahu@kalvium.community>
2024-03-12 15:01:38 +0530

    Schema is created for the SignUp page

--- Serverside/Controller.js
+++ Serverside/Controller.js
@@ -1,3 +1,22 @@
+const mongoose = require('mongoose')
+const UserDataSignUp = require("./usermodel.js")
+const cors = require('cors');
+
+
+exports.startDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.URL, {dbName:process.env.DBNAME});
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+exports.anuj = (req,res) =>{
+    UserDataSignUp.find({})
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json({ error: err }));
+}
 
 
 
@@ -6,7 +25,13 @@ exports.homeRoute = (req,res)=>{
     res.send("I am the home route")
 }
 
+
+
+
 // Defining the route for the add_Users.
-exports.add_User = (req,res)=>{
-    res.send("Post Api is working fine.")
-}
+// exports.add_User = async (req,res)=>{
+//     const newUser = await UserDataSignUp.create(req.body);
+//     res.send(newUser)
+
+// }
+
