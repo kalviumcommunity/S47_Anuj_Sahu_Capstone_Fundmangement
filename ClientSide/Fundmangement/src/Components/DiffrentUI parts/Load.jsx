2a84c71a783feaae272830ff05ddfa034a01847b
Anuj004 <anuj.sahu@kalvium.community>
2024-04-15 17:03:32 +0530

    Fix the design issues, and create new design

---
+++ ClientSide/Fundmangement/src/Components/DiffrentUI parts/Load.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from './Loder/Loader'
+
+function Load() {
+    return (
+        <div>
+            <Loader/>
+        </div>
+    )
+}
+
+export default Load
